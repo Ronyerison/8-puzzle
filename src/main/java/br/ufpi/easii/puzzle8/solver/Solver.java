32bc4be25457992cb82bdc317be909ff57c3c199
Ronyerison <ronyerisonbraga@gmail.com>
2016-06-06 16:06:58 -0300

    Atualização classe solver

--- src/main/java/br/ufpi/easii/puzzle8/solver/Solver.java
+++ src/main/java/br/ufpi/easii/puzzle8/solver/Solver.java
@@ -10,11 +10,15 @@ import br.ufpi.easii.puzzle8.model.Board;
 public class Solver {
 	private Board boardInicial;
 	private List<Board> solution;
+	public int count;
+	public int maxFront;
 
 	
 	public Solver(Board inicial) {
 		this.boardInicial = inicial;
 		this.solution = new ArrayList<Board>();
+		this.count = 0;
+		this.maxFront = 0;
 	}
 	
 	public List<Board> solv(){
@@ -22,6 +26,8 @@ public class Solver {
 		List<Board> front = new ArrayList<Board>();
 		Board actual = boardInicial.clone();
 		visited.add(actual);
+		this.count = 0;
+		this.maxFront = 0;
 		while(!actual.isGoal()){
 			for (Board neighbor : actual.expandNeighbors()) {
 				if (!visited.contains(neighbor) && !front.contains(neighbor)) {
@@ -32,6 +38,10 @@ public class Solver {
 			actual = front.get(0).clone();
 			front.remove(0);
 			visited.add(actual);
+			count++;
+			if (front.size() > maxFront) {
+				maxFront = front.size();
+			}
 		}
 		
 		if(actual.isGoal()){
@@ -51,7 +61,8 @@ public class Solver {
 		Board actual = boardInicial.clone();
 		actual.setTotalCost(actual.getManhattan() * actual.getOutNumbersSum());
 		visited.add(actual);
-		int iterations = 0;
+		this.count = 0;
+		this.maxFront = 0;
 		while(!actual.isGoal()){
 			for (Board neighbor : actual.expandNeighbors()) {
 				if (!visited.contains(neighbor) && !front.contains(neighbor)) {
@@ -63,7 +74,10 @@ public class Solver {
 			actual = front.get(0).clone();
 			front.remove(0);
 			visited.add(actual);
-			iterations++;
+			count++;
+			if (front.size() > maxFront) {
+				maxFront = front.size();
+			}
 		}
 		
 		if(actual.isGoal()){
@@ -72,13 +86,81 @@ public class Solver {
 				actual = actual.getParent();
 			}
 			solution.add(boardInicial);
-			System.out.println("Número de execuções: " + iterations);
 			return solution;
 		}
 		
 		return null;
 	}
 	
+	public List<Board> breadthSearch(){
+		List<Board> visited = new ArrayList<Board>();
+		List<Board> front = new ArrayList<Board>();
+		Board actual = boardInicial.clone();
+		actual.setTotalCost(actual.getManhattan() * actual.getOutNumbersSum());
+		visited.add(actual);
+		this.count = 0;
+		this.maxFront = 0;
+		while(!actual.isGoal()){
+			for (Board neighbor : actual.expandNeighbors()) {
+				if (!visited.contains(neighbor) && !front.contains(neighbor)) {
+					front.add(neighbor);
+				}
+			}
+			actual = front.get(0).clone();
+			front.remove(0);
+			visited.add(actual);
+			count++;
+			if (front.size() > maxFront) {
+				maxFront = front.size();
+			}
+		}
+		
+		if(actual.isGoal()){
+			while(actual.getParent() != null){
+				this.solution.add(actual);
+				actual = actual.getParent();
+			}
+			solution.add(boardInicial);
+			return solution;
+		}
+		
+		return null;
+	}
+	
+	public List<Board> depthSearch(){
+		List<Board> visited = new ArrayList<Board>();
+		List<Board> front = new ArrayList<Board>();
+		Board actual = boardInicial.clone();
+		actual.setTotalCost(actual.getManhattan() * actual.getOutNumbersSum());
+		visited.add(actual);
+		this.count = 0;
+		this.maxFront = 0;
+		while(!actual.isGoal()){
+			for (Board neighbor : actual.expandNeighbors()) {
+				if (!visited.contains(neighbor) && !front.contains(neighbor)) {
+					front.add(neighbor);
+				}
+			}
+			actual = front.get(front.size()-1).clone();
+			front.remove(front.size()-1);
+			visited.add(actual);
+			count++;
+			if (front.size() > maxFront) {
+				maxFront = front.size();
+			}
+		}
+		
+		if(actual.isGoal()){
+			while(actual.getParent() != null){
+				this.solution.add(actual);
+				actual = actual.getParent();
+			}
+			solution.add(boardInicial);
+			return solution;
+		}
+		
+		return null;
+	}
 	
 	public void sort(List<Board> boards){
 		Collections.sort(boards, new Comparator<Board>() {
